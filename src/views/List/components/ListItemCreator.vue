e213ee7bab3ad61d5348f4404132b79931edf5ba
patrykwicher <patrykwicher6@gmail.com>
2022-01-30 23:29:36 +0100

    added transition to component

--- src/views/List/components/ListItemCreator.vue
+++ src/views/List/components/ListItemCreator.vue
@@ -1,51 +1,53 @@
 <template>
-   <div class="modal" :class="{ 'is-active': row.isVisible }">
-      <div class="modal-background" @click="closeModal"></div>
-      <div class="modal-content p-5 has-border">
-         <div class="field">
-            <label class="label">Name</label>
-            <div class="control">
-               <input
-                  type="text"
-                  class="input"
-                  placeholder="Name"
-                  v-model="item.name"
-               />
+   <transition name="fade" mode="in-out">
+      <div class="modal is-active" v-if="row.isVisible">
+         <div class="modal-background" @click="closeModal"></div>
+         <div class="modal-content p-5 has-border">
+            <div class="field">
+               <label class="label">Name</label>
+               <div class="control">
+                  <input
+                     type="text"
+                     class="input"
+                     placeholder="Name"
+                     v-model="item.name"
+                  />
+               </div>
             </div>
-         </div>
-         <div class="field">
-            <label class="label">Status</label>
-            <div class="control">
-               <div class="select">
-                  <select v-model="item.status">
-                     <option selected disabled>{{ item.status }}</option>
-                     <option>unverified</option>
-                     <option>verified</option>
-                  </select>
+            <div class="field">
+               <label class="label">Status</label>
+               <div class="control">
+                  <div class="select">
+                     <select v-model="item.status">
+                        <option selected disabled>{{ item.status }}</option>
+                        <option>unverified</option>
+                        <option>verified</option>
+                     </select>
+                  </div>
+               </div>
+            </div>
+            <div class="field is-grouped is-grouped-right">
+               <div class="control">
+                  <button
+                     class="button is-primary"
+                     @click="emitChangedItem"
+                     :disabled="isSubmitDisabled"
+                  >
+                     Submit
+                  </button>
+               </div>
+               <div class="control">
+                  <button class="button" @click="closeModal">Cancel</button>
                </div>
             </div>
          </div>
-         <div class="field is-grouped is-grouped-right">
-            <div class="control">
-               <button
-                  class="button is-primary"
-                  @click="emitChangedItem"
-                  :disabled="isSubmitDisabled"
-               >
-                  Submit
-               </button>
-            </div>
-            <div class="control">
-               <button class="button" @click="closeModal">Cancel</button>
-            </div>
-         </div>
+         <button
+            class="modal-close is-large"
+            aria-label="close"
+            @click="closeModal"
+         ></button>
       </div>
-      <button
-         class="modal-close is-large"
-         aria-label="close"
-         @click="closeModal"
-      ></button>
-   </div>
+   </transition>
 </template>
 <script>
 import { toRefs, computed } from 'vue'

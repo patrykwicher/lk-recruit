9766abc1ba78b786dc98f61954f80f1e7c543f4d
patrykwicher <patrykwicher6@gmail.com>
2022-01-30 23:27:41 +0100

    fixed tests

--- tests/unit/table.spec.js
+++ tests/unit/table.spec.js
@@ -5,15 +5,15 @@ describe('Check table component', () => {
    const content = [{ foo: 'Foo data 1' }]
    const wrapper = shallowMount(Table, { props: { config, content } })
    const rows = wrapper.findAll('.table-row')
-   const headers = wrapper.findAll('.table-top')
+   const headers = wrapper.findAll('.table-header')
    it('Renders correctly', async () => {
-      expect(rows.length).toBe(4)
+      expect(rows.length).toBe(1)
       expect(headers[0].text()).toBe(config.columns[0].header)
       expect(rows[0].text()).toBe(content[0].foo)
    })
    it('Emits on select', async () => {
       await rows[0].trigger('click')
-      const emit = wrapper.emitted('click')[0]
+      const emit = wrapper.emitted('select')[0]
       expect(emit[0]).toEqual(content[0])
    })
 })
